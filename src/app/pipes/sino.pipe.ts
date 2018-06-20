a8961a826fd8bb20133f25113613c1c52813714d
juanpabloletteri <pabloletteri@gmail.com>
2018-06-20 18:16:33 -0300

    seleccion en tabla

--- src/app/pipes/sino.pipe.ts
+++ src/app/pipes/sino.pipe.ts
@@ -6,13 +6,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class SinoPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    if(value==0){
+    if (value == 0) {
       return "No";
     }
-    else if(value==1){
+    else if (value == 1) {
       return "Si";
     }
-    else{
+    else {
       return value;
     }
   }
