7ce7ede7262ab7a9504bcbc26b50342087db8536
juanpabloletteri <pabloletteri@gmail.com>
2018-07-04 19:08:24 -0300

    escrive el servicio en el can activate

--- src/app/servicios/autenticacion/aut-chofer.service.ts
+++ src/app/servicios/autenticacion/aut-chofer.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
+import { DatosUsuarioService } from '../../servicios/datos-usuario.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AutChoferService {
 
-  constructor() { }
+  constructor(private datosUsuario: DatosUsuarioService) { }
+
   canActivate(route, state): boolean {
     var aprobacion = false;
     //capturo token del local storage
@@ -14,6 +16,12 @@ export class AutChoferService {
     let payload = data.split('.')[1];
     let pay2 = payload.replace('-', '+').replace('_', '/');
     let datos = JSON.parse(atob(pay2));
+    //cargo el servicio con los datos del usuario
+    this.datosUsuario.setIdUsuario(datos['data']['id_usuario']);
+    this.datosUsuario.setNombre(datos['data']['nombre']);
+    this.datosUsuario.setApellido(datos['data']['apellido']);
+    this.datosUsuario.setMail(datos['data']['mail']);
+    this.datosUsuario.setTipo(datos['data']['tipo']);
     //comparo y si el tipo coincide acepto
     if (datos['data']['tipo'] == 2) {
       aprobacion = true;
