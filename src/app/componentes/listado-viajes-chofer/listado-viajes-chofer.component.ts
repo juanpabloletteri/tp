7a1568b056903d3490e526c103c0a8e898f62b37
juanpabloletteri <pabloletteri@gmail.com>
2018-07-15 22:00:25 -0300

    muestra inicio destino y recorrido en mapa

--- src/app/componentes/listado-viajes-chofer/listado-viajes-chofer.component.ts
+++ src/app/componentes/listado-viajes-chofer/listado-viajes-chofer.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, NgZone, OnInit, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Viaje } from '../../clases/viaje';
 import { ViajesService } from '../../servicios/viajes.service';
 import { DatosUsuarioService } from '../../servicios/datos-usuario.service';
+import { } from '@types/googlemaps';
+import { MapsAPILoader } from '@agm/core';
 import swal from 'sweetalert2';
 
 @Component({
@@ -17,15 +20,40 @@ export class ListadoViajesChoferComponent implements OnInit {
   id_chofer: number;
   viajeSeleccionado: Viaje;
   estadoViaje: any;
+  visible: boolean = true;
 
-  constructor(private miServicioViaje: ViajesService, private datosUsuario: DatosUsuarioService, private rute: Router) {
+  public latitude: number;
+  public longitude: number;
+  public searchControl: FormControl;
+  public zoom: number = 14;
+
+  public latitude1: number;
+  public longitude1: number;
+
+  public latitude2: number;
+  public longitude2: number;
+
+  public lat: Number;
+  public lng: Number;
+
+  public dir: any = undefined;
+
+  public OriplaceGmaps: google.maps.places.PlaceResult;
+  public AuxPlaceGmaps: google.maps.places.PlaceResult;
+  public DestplaceGmaps: google.maps.places.PlaceResult;
+
+  @ViewChild("search")
+  public searchElementRef: ElementRef;
+
+  constructor(private mapsAPILoader: MapsAPILoader, private ngZone: NgZone, private miServicioViaje: ViajesService,
+    private datosUsuario: DatosUsuarioService, private rute: Router) {
+
 
     this.id_chofer = this.datosUsuario.getIdUsuario();
     this.miServicioViaje.traerViajesPorChofer(this.id_chofer)
       .then(data => {
         this.datosTabla = data;
       })
-
   }
 
   ngOnInit() {
@@ -45,10 +73,54 @@ export class ListadoViajesChoferComponent implements OnInit {
       { label: 'Canc chofer', value: '-4' },
       { label: 'Canc cliente', value: '-5' }
     ];
+    this.searchControl = new FormControl();
+  }
+
+  calcular() {
+    this.dir = {
+      origin: { lat: this.latitude1, lng: this.longitude1 },
+      destination: { lat: this.latitude2, lng: this.longitude2 }
+    }
+    //////////////////
+    let servicio = new google.maps.DistanceMatrixService();
+    let mode = google.maps.TravelMode['DRIVING'];
+    let origen = new google.maps.LatLng(this.latitude1, this.longitude1);
+    let destino = new google.maps.LatLng(this.latitude2, this.longitude2);
+    servicio.getDistanceMatrix({
+      origins: [origen],
+      destinations: [destino],
+      travelMode: mode,
+      unitSystem: google.maps.UnitSystem.METRIC,
+      durationInTraffic: true,
+      avoidHighways: false,
+      avoidTolls: false
+    }, (responseDis, status) => {
+      if (status !== google.maps.DistanceMatrixStatus.OK) {
+        console.log("error", status);
+      } else {
+        var distancia = responseDis.rows[0].elements[0].distance.value;
+        var tiempo = responseDis.rows[0].elements[0].duration.value;
+      }
+    });
+  }
+  //////////////////
+
+  verMapa(viaje) {
+    console.log(viaje);
+    this.visible = false;
+    this.latitude1 = viaje.latitud_inicio;
+    this.longitude1 = viaje.longitud_inicio;
+    this.latitude2 = viaje.latitud_destino;
+    this.longitude2 = viaje.longitud_destino;
+    this.calcular();
+  }
+
+  volver() {
+    this.visible = true;
   }
 
   onRowSelect(event) {
-    console.log(this.viajeSeleccionado.id_viaje);
+    //console.log(this.viajeSeleccionado.id_viaje);
     //this.display = true;
   }
 
